008843af1cdba31dc156a6aca2f7c38fe7ec90f2
Olatayo Tijesu <tjayolatayo@gmail.com>
2024-02-15 00:05:59 +0100

    Add drop down navbar

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,9 +1,37 @@
 import LightLogo from '../assets/icons/light-logo.svg';
 import navIcon from '../assets/icons/nav-icon.svg'
+import closeIcon from '../assets/icons/close.svg'
+import cartIcon from '../assets/icons/light_cart.svg'
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const handleNavToggle = () => {
+    setToggleNav(!toggleNav);
+  };
+
   return (
     <>
+      <nav className={`dropNav ${toggleNav ? 'active' : ''}`}>
+        <div className="rule">
+          <img 
+            onClick={handleNavToggle}
+            className= "closIcon" 
+            src={closeIcon} 
+            alt="Close" 
+          />
+          <ul>
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Our Blog</li>
+            <li>Contact us</li>
+            <li>Our academy</li>
+            <li>Planning</li>
+            <li><img src={cartIcon} alt="" /></li>
+          </ul>
+        </div>
+      </nav>
       <nav className="navbar rule">
         <ul className='topNav'>
           <li>
@@ -11,7 +39,11 @@ const Navbar = () => {
             <p>Diamonddreams Event</p>
           </li>
           <li>
-            <img src={navIcon} alt="Hamburger" />
+            <img 
+              src={navIcon} 
+              alt="Hamburger" 
+              onClick={handleNavToggle}
+            />
           </li>
         </ul>
       </nav>
