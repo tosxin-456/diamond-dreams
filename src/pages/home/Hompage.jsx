4c18b39545e419a9570a1fbe3a699d51153652f9
Olatayo Tijesu <tjayolatayo@gmail.com>
2024-02-14 14:52:40 +0100

    Add contact section

--- src/pages/home/Hompage.jsx
+++ src/pages/home/Hompage.jsx
@@ -14,6 +14,14 @@ import couple2 from '../../assets/images/couple2.png';
 import couple3 from '../../assets/images/couple3.jpg';
 import couple4 from '../../assets/images/couple4.jpg';
 
+import academyPic from '../../assets/images/expect.png';
+import whiteCouple from '../../assets/images/whiteCouple.jpeg';
+
+// Social Icons
+import addBook from '../../assets/icons/address-book.svg';
+import phoneIcon from '../../assets/icons/phone.svg';
+import mailIcon from '../../assets/icons/email.svg';
+
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 import 'swiper/css'
@@ -162,6 +170,108 @@ const Home = () => {
             <button className="swiper-button-prev" onClick={() => handleCoppleChange('prev', coupleSwiperRef)}></button>
             <button className="swiper-button-next" onClick={() => handleCoppleChange('next', coupleSwiperRef)}></button>
           </div>
+        </section>
+        <section className="academySection">
+          <h2>Our Academy</h2>
+          <img className='epectPic' src={academyPic} alt="What to Expect" />
+          <ol>
+            <li>
+              <b>Comprehensive Coursework:</b>
+              <ul><li>Diverse modules covering event planning, design, bridal fashion, and more.</li></ul>
+            </li>
+            <li>
+              <b>Continuous Support:</b>
+              <ul><li>Access to resources and ongoing assistance post-course completion.</li></ul>
+            </li>
+            <li>
+              <b>Expert Guidance:</b>
+              <ul><li>Insights and mentorship from industry professionals and seasoned experts.</li></ul>
+            </li>
+            <li>
+              <b>Hands-on Learning:</b>
+              <ul><li>Practical experience to apply theoretical knowledge in real-world scenarios.</li></ul>
+            </li>
+            <li>
+              <b>Flexible Learning Modes:</b>
+              <ul><li>In-person sessions, online modules, webinars, accommodating various schedules.</li></ul>
+            </li>
+            <li>
+              <b>Networking Opportunities:</b>
+              <ul><li>Connect with peers, mentors, and industry leaders, fostering valuable relationships.</li></ul>
+            </li>
+            <li>
+              <b>Certification Programs:</b>
+              <ul><li>Recognition of expertise with completion certificates.</li></ul>
+            </li>
+          </ol>
+        </section>
+        <section className="faq">
+          <h2>Frequently Asked Questions</h2>
+          <article>
+            <h3>What is the ordering process for wedding dresses or accessories?</h3>
+            <ul>
+              <li>The ordering process typically involves browsing our collection, selecting desired items, and placing an order either online or in-store.</li>
+            </ul>
+          </article>
+          <article>
+            <h3>Do you offer customization or alterations for dresses?</h3>
+            <ul>
+              <li>Yes, we provide customization and alterations to ensure your dress fits perfectly. Our skilled seamstresses will work with you to achieve your desired look.</li>
+            </ul>
+          </article>
+          <article>
+            <h3>What is the timeframe for ordering and receiving wedding attire?</h3>
+            <ul>
+              <li>The timeframe can vary based on the specific item and alterations required. We recommend ordering your attire at least 6-9 months in advance to allow sufficient time for fittings and alterations.</li>
+            </ul>
+          </article>
+          <article>
+            <h3>Can I view and try on dresses or accessories before making a purchase?</h3>
+            <ul>
+              <li>Yes, we encourage customers to visit our store to view and try on dresses and accessories before making a purchase.</li>
+            </ul>
+          </article>
+        </section>
+        <section className="blogSection">
+          <h2>Our Blog</h2>
+          <div className="blogContainer">
+            <article>
+              <img src={whiteCouple} alt="Blog Pic" />
+              <p>How to Choose the Perfect Wedding Venue</p>
+            </article>
+            <article>
+              <img src={whiteCouple} alt="Blog Pic" />
+              <p>Wedding Photography: Posing Ideas for the Perfect Shot</p>
+            </article>
+          </div>
+        </section>
+        <section className="contactLine">
+          <h2>Drop us a Line</h2>
+          <aside className="socialIne">
+            <div className="socialAdd">
+              <img src={addBook} alt="Contact" />
+              <p>Jos, Plateau State</p>
+            </div>
+            <div className="socialAdd">
+              <img src={phoneIcon} alt="Phone" />
+              <p>+234 07483463507</p>
+            </div>
+            <div className="socialAdd">
+              <img src={mailIcon} alt="Mail" />
+              <p>diamonddreams@gmail.com</p>
+            </div>
+          </aside>
+          <form>
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' />
+            <label htmlFor="phone">Phone</label>
+            <input type="number" id='phone' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' />
+            <label htmlFor="message">Message</label>
+            <textarea name="" id="message" cols="20" rows="6"></textarea>
+            <button>Send</button>
+          </form>
         </section>
       </main>
     </>
