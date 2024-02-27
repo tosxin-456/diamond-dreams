cd050c6ea66779d6f90bd0796e5c694951932dcf
Olatayo Tijesu <tjayolatayo@gmail.com>
2024-02-27 17:31:43 +0100

    Complete home desktop styles

--- src/pages/home/Hompage.jsx
+++ src/pages/home/Hompage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import SwiperCore from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -14,18 +14,21 @@ import couple3 from '../../assets/images/couple3.jpg';
 import couple4 from '../../assets/images/couple4.jpg';
 
 import academyPic from '../../assets/icons/expect.svg';
-import whiteCouple from '../../assets/images/whiteCouple.jpeg';
 
 // Social Icons
 import addBook from '../../assets/icons/address-book.svg';
 import phoneIcon from '../../assets/icons/phone.svg';
 import mailIcon from '../../assets/icons/email.svg';
+import lightAddBook from '../../assets/icons/book-light.svg';
+import lightPhoneIcon from '../../assets/icons/phone-light.svg';
+import lightMailIcon from '../../assets/icons/light_email.svg';
 
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import { useNavigate } from 'react-router-dom';
 
 SwiperCore.use([Autoplay, Navigation]);
 
@@ -72,10 +75,28 @@ const Home = () => {
 
   const collectionSwiperRef = useRef(null);
   const coupleSwiperRef = useRef(null);
+  
+  const phoneNumber = "+234 7048346350";
+  const history = useNavigate();
+  const [isMobile, setIsMobile] = useState(false);
 
-  return (
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return ( 
     <>
-      <section className='AboutSect'>
+      <section id='aboutUs' className='AboutSect'>
         <h2>About Us</h2>
         <p>
           Welcome to our wedding hub, where dreams come to life! Explore an exquisite collection of wedding dresses and accessories available for purchase or rent, curated to reflect individual styles and enhance your special day. Our comprehensive event planning services ensure a seamless and unforgettable wedding experience tailored to your unique vision. Additionally, our academy offers expert guidance and courses for aspiring wedding planners, empowering enthusiasts to craft extraordinary celebrations. Join us on this enchanting journey toward your perfect wedding day. Whether shopping for the perfect gown, planning your special day, or seeking to learn more, find everything you need in one place.
@@ -124,7 +145,7 @@ const Home = () => {
           </li>
         </ul>
       </section>
-      <section className='CouplesSect'>
+      <section id='testimonials' className='CouplesSect'>
         <h2>What do our couples say</h2>
         <div className='collectionSwiper'>
           <Swiper
@@ -179,7 +200,7 @@ const Home = () => {
           </li>
         </ol>
       </section>
-      <section className="faq">
+      <section id='faqs' className="faq">
         <h2>Frequently Asked Questions</h2>
         <div className="firstArtSect">
           <article>
@@ -229,35 +250,52 @@ const Home = () => {
       </section>
       <section className="contactLine">
         <h2>Drop us a Line</h2>
-        <aside className="socialIne">
-          <div className="socialAdd">
-            <img src={addBook} alt="Contact" />
-            <p>Jos, Plateau State</p>
-          </div>
-          <div className="socialAdd">
-            <img src={phoneIcon} alt="Phone" />
-            <p>+234 07483463507</p>
-          </div>
-          <div className="socialAdd">
-            <img src={mailIcon} alt="Mail" />
-            <p>diamonddreams@gmail.com</p>
-          </div>
-        </aside>
-        <form>
-          <label htmlFor="name">Name</label>
-          <input type="text" id='name' />
-          <label htmlFor="phone">Phone</label>
-          <input type="number" id='phone' />
-          <label htmlFor="email">Email</label>
-          <input type="email" id='email' />
-          <label htmlFor="message">Message</label>
-          <textarea name="" id="message" cols="20" rows="6"></textarea>
-          <button>Send</button>
-        </form>
+        <div className="contactConTainer">
+          {isMobile && (<aside className="socialIne">
+            <div className="socialAdd">
+              <img src={addBook} alt="Contact" />
+              <p>Jos, Plateau State</p>
+            </div>
+            <div className="socialAdd">
+              <img src={phoneIcon} alt="Phone" />
+              <p>{phoneNumber}</p>
+            </div>
+            <div className="socialAdd">
+              <img src={mailIcon} alt="Mail" />
+              <p>diamonddreams@gmail.com</p>
+            </div>
+          </aside>)}
+          {!isMobile && (<aside className="socialIne">
+            <div className="socialAdd">
+              <img src={lightAddBook} alt="Contact" />
+              <p>Jos, Plateau State</p>
+            </div>
+            <div className="socialAdd">
+              <img src={lightPhoneIcon} alt="Phone" />
+              <p>{phoneNumber}</p>
+            </div>
+            <div className="socialAdd">
+              <img src={lightMailIcon} alt="Mail" />
+              <p>diamonddreams@gmail.com</p>
+            </div>
+          </aside>)}
+          <form>
+            {!isMobile && (<h3>Send us a Message</h3>)}
+            <label htmlFor="name">Name</label>
+            <input type="text" id='name' />
+            <label htmlFor="phone">Phone</label>
+            <input type="number" id='phone' />
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' />
+            <label htmlFor="message">Message</label>
+            <textarea name="" id="message" cols="20" rows="6"></textarea>
+            <button>Send</button>
+          </form>
+        </div>
       </section>
       <section className="cta homeCta">
         <p>Experience the Elegance of Our Picked Pieces</p>
-        <button>Shop Now</button>
+        <button onClick={() => history('Shop')}>Shop Now</button>
       </section>
     </>
   );
